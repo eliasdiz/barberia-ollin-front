d0ef17c7c0a041460b600681cdfda3c4a18c08b7
Luis Hernandez <125385157+LuisH05@users.noreply.github.com>
2023-11-18 18:00:29 -0500

    agregando carrusel (#2)
    * levantando front
    
    * agregando carrausel

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Carousel from './components/Carousel'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <header className="h-[50vh] flex justify-center mt-10">
+      <div className=' flex justify-center items-center w-1/4 bg-white mr-5 rounded-lg'>
+        <h1 className='font-bold text-2xl'> Logo Barberia Ollin</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <Carousel/>
+      </header>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  mode: 'jit',
-  purge: [
-  './src/**/*.{js,jsx,ts,tsx}'
-  ],
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
+module.exports = withMT({
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-  extend: {
-    
-    screens:{
-      'xxsm': {'min': '320px', 'max': '427px'},
-      'xsm':{'min': '427px', 'max': '1002px'},
-      'md': {'min': '1002px', 'max': '4000px'}
-      
-    },
-
-      colors:{
-      primary: "#222",
-      secundary: "#fff",
-      acento: "#222",
-    },
-  
-      fontFamily: {
-        'montserrat': ['Montserrat'],
-    }
-  }
+    extend: {},
   },
-  variants: {},
-  plugins: []
-}
+  plugins: [],
+});
