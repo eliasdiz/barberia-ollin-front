f8bc0ccac50817d33c8fc1a6a293cbe891ead1eb
Luis Hernandez <125385157+LuisH05@users.noreply.github.com>
2024-01-18 09:02:53 -0500

    HEADER MOBILE (#12)
    * levantando front
    
    * agregando carrausel
    
    * reseña y direccion
    
    * reseña y direccion
    
    * restructurando el contenido del proyecto
    
    * carusel de los barberos
    
    * corrigiendo responsive y agregando redes sociales
    
    * nada
    
    * header mobile
    
    * home mobile

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,7 +2,7 @@ import { Carousel } from "@material-tailwind/react";
 
 function CarouselOne() {
   return (
-    <Carousel className=" w-[30rem] h-full rounded-lg" loop>
+    <Carousel className=" w-[35rem] h-full rounded-lg" autoplay loop>
       <img
         src="https://images.pexels.com/photos/1813272/pexels-photo-1813272.jpeg?auto=compress&cs=tinysrgb&w=1600"
         alt="image 1"
@@ -20,37 +20,37 @@ function CarouselOne() {
       />
       <img
         src="https://i.postimg.cc/x15XVHRR/IMG-20231129-WA0045.jpg"
-        alt="image 3"
+        alt="image 4"
         className="h-full w-full object-cover"
       />
       <img
         src="https://i.postimg.cc/HsGcwTnQ/IMG-20231129-WA0047.jpg"
-        alt="image 3"
+        alt="image 5"
         className="h-full w-full object-cover"
       />
       <img
         src="https://i.postimg.cc/ydMdyT1V/IMG-20231129-WA0048.jpg"
-        alt="image 3"
+        alt="image 6"
         className="h-full w-full object-cover"
       />
       <img
         src="https://i.postimg.cc/Bn4jzNxb/IMG-20231129-WA0049.jpg"
-        alt="image 3"
+        alt="image 7"
         className="h-full w-full object-cover"
       />
       <img
         src="https://i.postimg.cc/g0BrnW1L/IMG-20231129-WA0050.jpg"
-        alt="image 3"
+        alt="image 8"
         className="h-full w-full object-cover"
       />
       <img
         src="https://i.postimg.cc/vmmcYB2d/IMG-20231129-WA0051.jpg"
-        alt="image 3"
+        alt="image 9"
         className="h-full w-full object-cover"
       />
       <img
         src="https://i.postimg.cc/Zn6q7yn6/IMG-20231129-WA0052.jpg"
-        alt="image 3"
+        alt="image 10"
         className="h-full w-full object-cover"
       />
     </Carousel>
