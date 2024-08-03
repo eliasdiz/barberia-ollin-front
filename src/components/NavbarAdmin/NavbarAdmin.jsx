93ed453042332d7150ab61c15cd5cec3dda22787
eliasdiz <114044365+eliasdiz@users.noreply.github.com>
2024-08-02 19:27:33 -0500

    Elias (#34)
    * cambios elias
    
    * reloj abierto cerrado
    
    * VISTA ADMIN: panel de usuarios, barberos y clientes, buscador funcional
    
    * fix
    
    * redux
    
    * card de informacion de usuario con boton de editar
    
    * redux: para capturar los id de las diferentes colecciones
    
    * Usuario: ver el usuario seleccionado
    
    * Usuarios: funcionalidad del controlador del backend para eliminar un usuario
    
    * Layouts: Navbar admin
    
    * Panel Usuarios: cambios en las vistas responsive

--- src/components/NavbarAdmin/NavbarAdmin.jsx
+++ src/components/NavbarAdmin/NavbarAdmin.jsx
@@ -1,167 +1,50 @@
-    import React, { useEffect } from "react";
-    import {Navbar,Collapse,Typography,Button,IconButton,List,ListItem,Menu,MenuHandler,MenuList,MenuItem,} from "@material-tailwind/react";
-    import {ChevronDownIcon,Bars3Icon,XMarkIcon,} from "@heroicons/react/24/outline";
-    import {Bars4Icon,GlobeAmericasIcon,NewspaperIcon,PhoneIcon,RectangleGroupIcon,SquaresPlusIcon,SunIcon,TagIcon,UserGroupIcon,} from "@heroicons/react/24/solid";
+import React, { useEffect } from "react";
+import {Navbar,Collapse,Typography,Button,IconButton,List,ListItem,Menu,MenuHandler,MenuList,MenuItem,} from "@material-tailwind/react";
+import {ChevronDownIcon,Bars3Icon,XMarkIcon} from "@heroicons/react/24/outline";
+import {Bars4Icon,GlobeAmericasIcon,NewspaperIcon,PhoneIcon,RectangleGroupIcon,SquaresPlusIcon,SunIcon,TagIcon,UserGroupIcon,} from "@heroicons/react/24/solid";
+import { Link, useNavigate } from "react-router-dom";
+import { HiMiniHome } from "react-icons/hi2";
+import { BsFillPeopleFill } from "react-icons/bs";
+
     
-    const navListMenuItems = [
-    {
-        title: "Products",
-        description: "Find the perfect solution for your needs.",
-        icon: SquaresPlusIcon,
-    },
-    {
-        title: "About Us",
-        description: "Meet and learn about our dedication",
-        icon: UserGroupIcon,
-    },
-    {
-        title: "Blog",
-        description: "Find the perfect solution for your needs.",
-        icon: Bars4Icon,
-    },
-    {
-        title: "Services",
-        description: "Learn how we can help you achieve your goals.",
-        icon: SunIcon,
-    },
-    {
-        title: "Support",
-        description: "Reach out to us for assistance or inquiries",
-        icon: GlobeAmericasIcon,
-    },
-    {
-        title: "Contact",
-        description: "Find the perfect solution for your needs.",
-        icon: PhoneIcon,
-    },
-    {
-        title: "News",
-        description: "Read insightful articles, tips, and expert opinions.",
-        icon: NewspaperIcon,
-    },
-    {
-        title: "Products",
-        description: "Find the perfect solution for your needs.",
-        icon: RectangleGroupIcon,
-    },
-    {
-        title: "Special Offers",
-        description: "Explore limited-time deals and bundles",
-        icon: TagIcon,
-    },
-    ];
     
-    function NavListMenu() {
-    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-    const [isMobileMenuOpen, setIsMobileMenuOpen] = React.useState(false);
-    const renderItems = navListMenuItems.map(
-        ({ icon, title, description }, key) => (
-        <a href="#" key={key}>
-            <MenuItem className="flex items-center gap-3 rounded-lg">
-            <div className="flex items-center justify-center rounded-lg !bg-blue-gray-50 p-2 ">
-                {" "}
-                {React.createElement(icon, {
-                strokeWidth: 2,
-                className: "h-6 text-gray-900 w-6",
-                })}
-            </div>
-            <div>
-                <Typography
-                variant="h6"
-                color="blue-gray"
-                className="flex items-center text-sm font-bold"
-                >
-                {title}
-                </Typography>
-                <Typography
-                variant="paragraph"
-                className="text-xs !font-medium text-blue-gray-500"
-                >
-                {description}
-                </Typography>
-            </div>
-            </MenuItem>
-        </a>
-        ),
-    );
-    
-    return (
-        <React.Fragment>
-        <Menu
-            open={isMenuOpen}
-            handler={setIsMenuOpen}
-            offset={{ mainAxis: 20 }}
-            placement="bottom"
-            allowHover={true}
-        >
-            <MenuHandler>
-            <Typography as="div" variant="small" className="font-medium">
-                <ListItem
-                className="flex items-center gap-2 py-2 pr-4 font-medium text-gray-900"
-                selected={isMenuOpen || isMobileMenuOpen}
-                onClick={() => setIsMobileMenuOpen((cur) => !cur)}
-                >
-                Resources
-                <ChevronDownIcon
-                    strokeWidth={2.5}
-                    className={`hidden h-3 w-3 transition-transform lg:block ${
-                    isMenuOpen ? "rotate-180" : ""
-                    }`}
-                />
-                <ChevronDownIcon
-                    strokeWidth={2.5}
-                    className={`block h-3 w-3 transition-transform lg:hidden ${
-                    isMobileMenuOpen ? "rotate-180" : ""
-                    }`}
-                />
-                </ListItem>
-            </Typography>
-            </MenuHandler>
-            <MenuList className="hidden max-w-screen-xl rounded-xl lg:block">
-            <ul className="grid grid-cols-3 gap-y-2 outline-none outline-0">
-                {renderItems}
-            </ul>
-            </MenuList>
-        </Menu>
-        <div className="block lg:hidden">
-            <Collapse open={isMobileMenuOpen}>{renderItems}</Collapse>
-        </div>
-        </React.Fragment>
-    );
-    }
+export default function NavbarAdmin() {
+            
+    const [openNav, setOpenNav] = React.useState(false);
     
     function NavList() {
     return (
         <List className="mt-4 mb-6 p-0 lg:mt-0 lg:mb-0 lg:flex-row lg:p-1">
-            <Typography
-                as="a"
-                href="#"
-                variant="small"
-                color="blue-gray"
-                className="font-medium"
-            >
-            <ListItem className="flex items-center gap-2 py-2 pr-4">Home</ListItem>
-            </Typography>
-            <NavListMenu />
-            <Typography
-                as="a"
-                href="#"
-                variant="small"
-                color="blue-gray"
-                className="font-medium"
-            >
-                <ListItem className="flex items-center gap-2 py-2 pr-4">
-                Contact Us
+            <Link to={'/'}>
+                <ListItem className=" ">
+                    <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="flex items-center gap-2 font-medium capitalize"
+                    >
+                        <HiMiniHome className="h-4 w-4" />
+                        inicio
+                    </Typography>
                 </ListItem>
-            </Typography>
+            </Link>
+
+            <Link to={'/admin/usuarios'}>
+                <ListItem className=" ">
+                    <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="flex items-center gap-2 font-medium capitalize"
+                    >
+                        <BsFillPeopleFill className="h-4 w-4" />
+                        usuarios
+                    </Typography>
+                </ListItem>
+            </Link>
+
+            
         </List>
     );
     }
-    
-    export default function NavbarAdmin() {
-
-    const [openNav, setOpenNav] = React.useState(false);
-    
     useEffect(() => {
         window.addEventListener(
         "resize",
@@ -170,51 +53,51 @@
     }, []);
     
     return (
-        <Navbar className="sticky top-0 mx-auto max-w-screen-xl px-4 py-2 shadow-md z-50" color="blue-gray">
-        <div className="flex items-center justify-between text-blue-gray-900">
-            <Typography
-            as="a"
-            href="#"
-            variant="h6"
-            className="mr-4  cursor-pointer py-1.5 lg:ml-2 uppercase"
-            >
-            menu
-            </Typography>
-            <div className="hidden lg:block">
-            <NavList />
+        <Navbar className="mx-auto max-w-screen-xl px-4 py-2 shadow-md md:w-[60%] " color="blue-gray">
+            <div className="flex items-center justify-between text-blue-gray-900">
+                <Typography
+                as="a"
+                href="#"
+                variant="h6"
+                className="mr-4  cursor-pointer py-1.5 lg:ml-2 uppercase"
+                >
+                menu
+                </Typography>
+                <div className="hidden lg:block">
+                <NavList />
+                </div>
+                <div className="hidden gap-2 lg:flex">
+                <Button variant="gradient" size="sm" color="blue-gray">
+                    iniciar sesion
+                </Button>
+                <Button variant="gradient" size="sm">
+                    cerrar sesion
+                </Button>
+                </div>
+                <IconButton
+                variant="text"
+                color="blue-gray"
+                className="lg:hidden"
+                onClick={() => setOpenNav(!openNav)}
+                >
+                {openNav ? (
+                    <XMarkIcon className="h-6 w-6 text-black" strokeWidth={2} />
+                ) : (
+                    <Bars3Icon className="h-6 w-6 text-black" strokeWidth={2} />
+                )}
+                </IconButton>
             </div>
-            <div className="hidden gap-2 lg:flex">
-            <Button variant="gradient" size="sm" color="blue-gray">
-                Log In
-            </Button>
-            <Button variant="gradient" size="sm">
-                Sign In
-            </Button>
-            </div>
-            <IconButton
-            variant="text"
-            color="blue-gray"
-            className="lg:hidden"
-            onClick={() => setOpenNav(!openNav)}
-            >
-            {openNav ? (
-                <XMarkIcon className="h-6 w-6 text-black" strokeWidth={2} />
-            ) : (
-                <Bars3Icon className="h-6 w-6 text-black" strokeWidth={2} />
-            )}
-            </IconButton>
-        </div>
-        <Collapse open={openNav}>
-            <NavList />
-            <div className="flex w-full flex-nowrap items-center gap-2 lg:hidden">
-            <Button variant="gradient" size="sm" color="blue-gray" fullWidth>
-                Log In
-            </Button>
-            <Button variant="gradient" size="sm" fullWidth>
-                Sign In
-            </Button>
-            </div>
-        </Collapse>
+            <Collapse open={openNav}>
+                <NavList />
+                <div className="flex w-full flex-nowrap items-center gap-2 lg:hidden">
+                <Button variant="gradient" size="sm" color="blue-gray" fullWidth>
+                    iniciar sesion
+                </Button>
+                <Button variant="gradient" size="sm" fullWidth>
+                    cerrar sesion
+                </Button>
+                </div>
+            </Collapse>
         </Navbar>
-    );
-    }
+);
+}
--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Footer from '../components/footer'
 import { Outlet } from 'react-router-dom';
+import NavbarAdmin from '../components/NavbarAdmin/NavbarAdmin';
+
+
 export default function Main() {
+
+  
   return (
     <>
+      <NavbarAdmin />
       <Outlet />
       <Footer />
     </>
