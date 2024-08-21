62e98715c0dab9693129e2818691a7ba6fc14acd
eliasdiz <114044365+eliasdiz@users.noreply.github.com>
2024-08-21 02:27:48 -0500

    Elias (#51)
    * resgistro: vista del registro de usuarios
    
    * Usuarios: registro de usuarios

--- src/components/FormRegistro/FormRegistro.jsx
+++ src/components/FormRegistro/FormRegistro.jsx
@@ -1,23 +1,58 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import InputTexto from '../InputTexto/InputTexto'
 import { Button } from '@material-tailwind/react'
-import { Toaster } from 'react-hot-toast'
+import toast, { Toaster } from 'react-hot-toast'
 import { TbAbc } from "react-icons/tb";
 import { HiOutlineDevicePhoneMobile } from "react-icons/hi2";
 import { FiAtSign } from "react-icons/fi";
+import axios from 'axios';
+import { urlLocal } from '../../urlHost';
+import { useNavigate } from 'react-router-dom';
+
 
 
 export default function FormRegistro() {
 
+    const navigate = useNavigate()
+    const nombres = useRef()
+    const apellidos = useRef()
+    const email = useRef()
+    const telefono = useRef()
+
+
     const hanldeRegistro = () => {
-        
+        let data = {
+            nombres: nombres.current.value.toLowerCase(),
+            apellidos: apellidos.current.value.toLowerCase(),
+            email: email.current.value.toLowerCase(),
+            telefono: telefono.current.value,
+            password: ''
+        }
+        // console.log(data)
+        const promesa = axios.post(`${urlLocal}usuarios/crear`,data) 
+        toast.promise(
+            promesa,
+            {
+                loading: 'creando usuario',
+                success: (res) =>{
+                    setTimeout(() => {
+                        navigate('/')
+                    }, 2000);
+                    return <>{res.data.message}</>
+                },
+                error: (error) => {
+                    return <>{error.response.data.message}</>
+                }
+            },{
+                style: { background: '#94a3b8', textTransform: 'capitalize', color: 'black'},
+            }
+        )
     }
 
     return (
-        <div className='flex justify-center p-5'>
+        <div className='flex justify-center p-4'>
             <div
-                className="flex flex-col gap-2 shadow-lg bg-blue-gray-400 px-4 sm:px-6 md:px-8 lg:px-10 py-8 rounded-xl w-full max-w-md"
-                onClick={hanldeRegistro}
+                className="flex flex-col shadow-lg bg-blue-gray-400 px-4 sm:px-6 md:px-8 lg:px-10 py-8 rounded-xl w-full max-w-md"
             >
                 <div 
                     className='font-bold uppercase text-center p-2'
@@ -28,24 +63,25 @@ export default function FormRegistro() {
                     <InputTexto 
                         icono={<TbAbc className='w-7 h-7' />}
                         placeHold='nombre' 
-                        // parentRef={usuario}
+                        parentRef={nombres}
+                        className='capitalize border-2 border-black'
                     />
 
                     <InputTexto 
                         icono={<TbAbc className='w-7 h-7' />}
                         placeHold='apellido' 
-                        // parentRef={usuario}
+                        parentRef={apellidos}
                     />
                     <InputTexto 
                         placeHold='email' 
-                        icono={<FiAtSign className='w-7 h.7' />}
-                        // parentRef={usuario}
+                        icono={<FiAtSign className='w-7 h.7 ' />}
+                        parentRef={email}
                     />
                     <InputTexto 
                         placeHold='telefono'
                         tipo='number'
                         icono={<HiOutlineDevicePhoneMobile className='w-6 h-6' />}
-                        // parentRef={usuario}
+                        parentRef={telefono}
                     />
                     <div className="flex w-full">
                         <Button 
@@ -54,6 +90,7 @@ export default function FormRegistro() {
                             fullWidth
                             variant='gradient'
                             className="flex items-center justify-center gap-3 text-base "
+                            onClick={hanldeRegistro}
                         >
                             registrate
                             <span>
