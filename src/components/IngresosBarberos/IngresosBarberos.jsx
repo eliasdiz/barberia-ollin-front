b78360c3d71f6a66647c1d79177bebe50038b4b4
elias <eliasj.diz@gmail.com>
2024-09-19 22:02:55 -0500

    Vista Ingresos Barberos: tabla que muestra los ingresos de los barbero, total y un filtro por fecha

--- src/components/IngresosBarberos/IngresosBarberos.jsx
+++ src/components/IngresosBarberos/IngresosBarberos.jsx
@@ -4,6 +4,8 @@ import { format } from '@formkit/tempo'
 import actionsServicios from '../../Store/Servicios/actions.js'
 import { useDispatch } from 'react-redux'
 import numeral from 'numeral'
+import axios from 'axios'
+import { urlLocal } from '../../urlHost'
 
 
 const { getServicios } = actionsServicios
@@ -11,33 +13,29 @@ const { getServicios } = actionsServicios
 export default function IngresosBarberos() {
 
     const dispatch = useDispatch()
-
-    const [fecha, setFecha] = useState('')
+    const token = localStorage.getItem('token')
+    const headers = { headers: { Authorization: `Bearer ${token}`}}
     
+    const [fecha, setFecha] = useState('')
     const tableHead = ["#", "Fecha", "Servicio", "Valor"]
-    const servicios = [
-        { id: 1, fecha: new Date("2023-05-01"), servicio: "Consultoría", valor: 25000 },
-        { id: 2, fecha: new Date("2023-05-03"), servicio: "Desarrollo web", valor: 10000 },
-        { id: 3, fecha: new Date("2023-05-05"), servicio: "Diseño gráfico", valor: 30000 },
-        { id: 4, fecha: new Date("2023-05-10"), servicio: "SEO", valor: 40000 },
-        { id: 5, fecha: new Date("2023-05-15"), servicio: "Marketing digital", valor: 60000 },
-    ]
+    const [ servicios, setServicios ] = useState([]) 
+    const total = servicios?.map(({valor}) => valor / 2)?.reduce((a,b) => a+b,0)
 
-    const serviciosFiltrados = fecha
-        ? servicios.filter(s => format(s.fecha, "yyyy-MM-dd") === fecha)
-        : servicios
-
-    const total = serviciosFiltrados.reduce((sum, servicio) => sum + servicio.valor, 0)
+    // console.log(servicios)
 
     useEffect(
         () => {
             dispatch(getServicios())
+            axios.get(`${urlLocal}ingresos/ingresos-barbero`,headers)
+                .then( res => setServicios(res.data.ingresos))
+                .catch( error => console.log(error))
         },
         []
     )
 
+
     return (
-        <Card className="w-full h-[89vh] max-w-4xl mx-auto flex flex-col justify-start p-1 mt-2  bg-gray-800 text-gray-100">
+        <Card className="w-full h-[89vh] md:w-[60%] max-w-4xl mx-auto flex flex-col justify-start p-1 mt-2  bg-gray-800 text-gray-100">
             <div className="flex flex-col gap-4 md:flex-row md:items-center md:justify-between p-3">
                 <div className="flex justify-between items-center">
                     <div className="w-[40%]">
@@ -59,50 +57,50 @@ export default function IngresosBarberos() {
                 </div>
             </div>
             <table className="w-full min-w-max table-auto text-left overflow-x-auto px-0">
-            <thead>
-                <tr>
+                <thead>
+                    <tr>
+                        {
+                            tableHead.map((head) => (
+                                <th key={head} className="border-b border-gray-700 bg-gray-800 p-4">
+                                <Typography
+                                    variant="small"
+                                    color="white"
+                                    className="font-normal leading-none opacity-70 uppercase"
+                                >
+                                    {head}
+                                </Typography>
+                                </th>
+                            ))
+                        }
+                    </tr>
+                </thead>
+                <tbody>
                     {
-                        tableHead.map((head) => (
-                            <th key={head} className="border-b border-gray-700 bg-gray-800 p-4">
-                            <Typography
-                                variant="small"
-                                color="white"
-                                className="font-normal leading-none opacity-70"
-                            >
-                                {head}
-                            </Typography>
-                            </th>
-                        ))
-                    }
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    serviciosFiltrados.map(({ id, fecha, servicio, valor }, i) => (
-                        <tr key={i} className={i % 2 === 0 ? "bg-gray-900" : ""}>
-                            <td className="p-4">
-                            <Typography variant="small" color="white" className="font-normal">
-                                {id}
-                            </Typography>
-                            </td>
-                            <td className="p-4">
-                            <Typography variant="small" color="white" className="font-normal">
-                                {format(fecha,'YYYY-MM-DD' )}
-                            </Typography>
-                            </td>
-                            <td className="p-4">
-                            <Typography variant="small" color="white" className="font-normal">
-                                {servicio}
-                            </Typography>
-                            </td>
-                            <td className="text-right p-4">
-                            <Typography variant="small" color="white" className="font-normal">
-                                $ {numeral(valor).format()}
-                            </Typography>
-                            </td>
-                        </tr>
-                ))}
-            </tbody>
+                        servicios?.map(({fecha, servicio, valor }, i) => (
+                            <tr key={i} className={i % 2 === 0 ? "bg-gray-900" : ""}>
+                                <td className="p-4">
+                                <Typography variant="small" color="white" className="font-normal">
+                                    {i + 1}
+                                </Typography>
+                                </td>
+                                <td className="p-4">
+                                <Typography variant="small" color="white" className="font-normal">
+                                    {format(fecha,'YYYY-MM-DD' )}
+                                </Typography>
+                                </td>
+                                <td className="p-4">
+                                <Typography variant="small" color="white" className="font-normal capitalize">
+                                    {servicio}
+                                </Typography>
+                                </td>
+                                <td className="p-4">
+                                <Typography variant="small" color="white" className="font-normal">
+                                    $ {numeral(valor / 2).format() }
+                                </Typography>
+                                </td>
+                            </tr>
+                    ))}
+                </tbody>
             </table>
         </Card>
     )
