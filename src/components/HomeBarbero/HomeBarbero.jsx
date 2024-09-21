15aa1df8fa7d8203c8f6866294fd3aebdebacabe
elias <eliasj.diz@gmail.com>
2024-09-21 01:48:38 -0500

    Home Barbero: vista de citas hoy y mañana

--- src/components/HomeBarbero/HomeBarbero.jsx
+++ src/components/HomeBarbero/HomeBarbero.jsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import actionsUsuarios from '../../Store/Usuarios/actions'
 import actionsServicios from '../../Store/Servicios/actions'
+import CardCitas from '../CardCitas/CardCitas'
 
 const { getServicios} = actionsServicios
 const { getUsuario} = actionsUsuarios
@@ -22,23 +23,23 @@ export default function HomeBarbero() {
     )
 
     return (
-        <div className='flex flex-col justify-center p-3 min-h-[69vh] capitalize'>
-            <Typography 
-                className='text-white text-center' 
-                variant='h3'
-            >
-                bienvenido!!
-            </Typography>
+        <div className='flex flex-col items-center min-h-[69vh] mt-2 p-1'>
+            <div className='w-full h-full md:w-[60%] flex flex-col gap-1'>
+                <Typography 
+                    className='text-white text-center capitalize' 
+                    variant='h5'
+                >
+                    bienvenido
+                </Typography>
+                <Typography 
+                    className='text-white text-center capitalize' 
+                    variant='h5'
+                >
+                    {usuario.nombres} {usuario.apellidos}
+                </Typography>
 
-            <Typography 
-                className='text-white text-center' 
-                variant='h3'
-            >
-                {
-                    usuario.barbero ? `${usuario.nombres} ${usuario.apellidos}` : '' 
-                }  
-            </Typography>
-            
+                <CardCitas />
+            </div>            
         </div>
     )
 }
