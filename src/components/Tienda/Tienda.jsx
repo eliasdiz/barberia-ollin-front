1b8dca8c127801659cb70896babb498089018d8b
elias <eliasj.diz@gmail.com>
2024-10-02 21:41:07 -0500

    Tienda: vista final de la tienda y los carritos

--- src/components/Tienda/Tienda.jsx
+++ src/components/Tienda/Tienda.jsx
@@ -1,7 +1,10 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import CrearCuenta from '../CrearCuenta/CrearCuenta'
 import actionsServicios from '../../Store/Servicios/actions'
 import { useDispatch } from 'react-redux'
+import axios from 'axios'
+import { urlLocal } from '../../urlHost'
+import CarritoCliente from '../CarritoCliente/CarritoCliente'
 
 
 const { getServicios} = actionsServicios
@@ -11,15 +14,42 @@ export default function Tienda() {
 
     const dispatch = useDispatch()
 
+    const [ carritos, setCarritos ] = useState([])
+
+    // console.log(carritos)
+
     useEffect(
         () => {
+            const token = localStorage.getItem('token')
+            const headers = { headers: { Authorization: `Bearer ${token}`}}
             dispatch(getServicios())
+            axios.get(`${urlLocal}carrito`,headers)
+                .then( res => setCarritos(res.data.carritos))
+                .catch( error => console.log(error))
         },
         []
     )
     return (
-        <div className='border flex justify-center mt-2'>
-            <CrearCuenta />
+        <div className='flex justify-center'>
+            <div className='w-[80%] flex flex-wrap justify-evenly gap-3 p-5 '>   
+                <div>
+                    <CrearCuenta />
+                </div>
+                {
+                    carritos.length !== 0 ?
+                        <>
+                            {
+                                carritos.map((item,i) => (
+                                    <CarritoCliente key={i} carrito={item} />
+                                ))
+                            }
+                        </>
+                        :
+                        <></>
+                }
+            </div>
         </div>
     )
 }
+
+
